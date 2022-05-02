d890b319b5bd4f5b5e86cf4d17d01a4ec6c0989a
VictoriaAnufrieva <victoria.anufrieva@gmail.com>
2022-05-02 18:24:39 +0200

    Add Courses.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css';
+import Courses from './components/Courses/Courses';
 import Header from './components/Header/Header';
 
 
@@ -6,6 +7,7 @@ function App() {
   return (
     <>
       <Header />
+      <Courses/>
     </>
   );
 }

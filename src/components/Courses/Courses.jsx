847eefeb7d0cf664be1a386dac90d617016249db
VictoriaAnufrieva <victoria.anufrieva@gmail.com>
2022-05-04 17:35:37 +0200

    Edit Courses.jsx

--- src/components/Courses/Courses.jsx
+++ src/components/Courses/Courses.jsx
@@ -1,6 +1,11 @@
+import { mockedAuthorsList, mockedCoursesList } from "../../constants"
+import CourseCard from "./components/CourseCard/CourseCard"
+
 export default function Courses() {
     return (
         <div>
+            <div></div>
+            <div>{mockedCoursesList.map(course => <CourseCard key={course.id} course={course}/>)}</div>
             
         </div>
     )

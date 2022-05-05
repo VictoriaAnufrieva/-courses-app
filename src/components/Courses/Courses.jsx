ecdc48bf7f2506dc6fea70b679acc2eb2c40756e
VictoriaAnufrieva <victoria.anufrieva@gmail.com>
2022-05-05 19:57:20 +0200

    added search

--- src/components/Courses/Courses.jsx
+++ src/components/Courses/Courses.jsx
@@ -1,13 +1,34 @@
-import { mockedAuthorsList, mockedCoursesList } from "../../constants"
-import CourseCard from "./components/CourseCard/CourseCard"
-import SearchBar from "./components/SearchBar/SearchBar"
+import { useState } from "react";
+import Button from "../../common/Button/Button";
+import { mockedAuthorsList, mockedCoursesList } from "../../constants";
+import CourseCard from "./components/CourseCard/CourseCard";
+import SearchBar from "./components/SearchBar/SearchBar";
 
 export default function Courses() {
-    return (
-        <div>
-            <div><SearchBar/></div>
-            <div>{mockedCoursesList.map(course => <CourseCard key={course.id} course={course}/>)}</div>
-            
-        </div>
-    )
+  const [searchValue, setSearchValue] = useState("");
+
+  return (
+    <div>
+      <div>
+        <SearchBar setSearchValue={setSearchValue} />
+    
+      </div>
+      <div>
+        {mockedCoursesList
+          .filter((course) =>
+            `${course.title} ${course.id}`.toLowerCase().includes(searchValue)
+          )
+          .map((course) => (
+            <CourseCard key={course.id} course={course} />
+          ))}
+      </div>
+    </div>
+  );
 }
+const test = [
+  { name: "Java", id: 1 },
+  { name: "JavaScript", id: 2 },
+  { name: "Pyton", id: 3 },
+];
+const value = "3";
+test.filter((el) => `${el.name} ${el.id}`.toLowerCase().includes(value));
--- src/components/Courses/components/SearchBar/SearchBar.jsx
+++ src/components/Courses/components/SearchBar/SearchBar.jsx
@@ -1,9 +1,15 @@
 import Button from "../../../../common/Button/Button";
 import Input from "../../../../common/Input/Input";
 
-export default function SearchBar() {
+export default function SearchBar({setSearchValue}) {
+    function handleSubmit(event){
+        event.preventDefault()
+        const form = event.target;
+        const searchValue = form.search.value.trim().toLowerCase()
+        setSearchValue(searchValue)
+    }
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <Input
         name="search"
         type="search"

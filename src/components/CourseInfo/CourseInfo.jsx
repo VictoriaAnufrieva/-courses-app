c20adb9356f5bcb05be7633552ab0b2da543f7e1
VictoriaAnufrieva <victoria.anufrieva@gmail.com>
2022-05-16 20:22:10 +0200

    Create CourseInfo.jsx

---
+++ src/components/CourseInfo/CourseInfo.jsx
@@ -0,0 +1,10 @@
+import Button from "../../common/Button/Button";
+
+export default function CourseInfo() {
+    return (
+        <>
+            <Button buttonText="Back to courses"/>
+            
+        </>
+    )
+}

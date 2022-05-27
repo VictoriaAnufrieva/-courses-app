ec0633de3b1262f6b9e63cd34ca785f16818d3d5
VictoriaAnufrieva <victoria.anufrieva@gmail.com>
2022-05-27 13:40:46 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { fetchCourses } from "./ services";
 import "./App.css";
 import CourseInfo from "./components/CourseInfo/CourseInfo";
 import Courses from "./components/Courses/Courses";
@@ -8,25 +10,30 @@ import Header from "./components/Header/Header";
 import Login from "./components/Login/Login";
 import Registration from "./components/Registration/Registration";
 import { mockedCoursesList } from "./constants";
+import { setCourses } from "./store/courses/actionCreators";
+import { coursesSelector } from "./store/courses/selectors";
 
 function App() {
-  const [courses, setCourses] = useState(mockedCoursesList);
+  const dispatch = useDispatch();
+  const courses = useSelector(coursesSelector);
+  // const [courses, setCourses] = useState(mockedCoursesList);
+  useEffect(() => {
+    fetchCourses().then((data) => {
+      dispatch(setCourses(data.result));
+    });
+  }, []);
+
   // const [isShownCreateCourse, setIsShownCreateCourse] = useState(false);
+
   return (
     <BrowserRouter>
       <Header />
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/registration" element={<Registration />} />
-        <Route
-          path="/courses/add"
-          element={<CreateCourse setCourses={setCourses} />}
-        />
-        <Route path="/courses" element={<Courses courses={courses} />} />
-        <Route
-          path="/courses/:courseId"
-          element={<CourseInfo courses={courses} />}
-        />
+        <Route path="/courses/add" element={<CreateCourse />} />
+        <Route path="/courses" element={<Courses />} />
+        <Route path="/courses/:courseId" element={<CourseInfo />} />
       </Routes>
       {/* {!isShownCreateCourse ? (
         <Courses

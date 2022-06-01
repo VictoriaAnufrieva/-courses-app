f286aa277961b0ba1ba8bb8510cc8c642e86f431
VictoriaAnufrieva <victoria.anufrieva@gmail.com>
2022-06-01 06:23:39 +0200

    Delete  services.js

--- src/ services.js
+++
@@ -1,9 +0,0 @@
-export function fetchCourses() {
-  return fetch("http://localhost:4000/courses/all")
-    .then((res) => res.json())
-    .then((data) => {
-       return data;
-      
-    })
-    .catch((error) => console.log(error));
-}

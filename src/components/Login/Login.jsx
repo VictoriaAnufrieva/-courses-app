ed56482aefcea08c45f99f2486f0d6152f90300e
VictoriaAnufrieva <victoria.anufrieva@gmail.com>
2022-06-01 06:22:18 +0200

    Update Login.jsx

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,33 +1,30 @@
+import { useDispatch } from "react-redux";
 import { Link, useNavigate } from "react-router-dom";
 import Button from "../../common/Button/Button";
 import Input from "../../common/Input/Input";
+import { fetchLogIn } from "../../services";
+import { logIn } from "../../store/user/actionCreators";
 
 export default function Login() {
-    const navigate = useNavigate()
-    function loginUser(event) {
-        event.preventDefault();
-        const form = event.target;
-        const userData = {
-          email: form.email.value,
-          password: form.password.value,
-        };
-        console.log(userData);
-        fetch("http://localhost:4000/login", {
-          method: "POST",
-          body: JSON.stringify(userData),
-          headers: {
-            "Content-Type": "application/json",
-          },
-        })
-          .then((res) => res.json())
-          .then((data) => {
-            window.localStorage.setItem('token', data.result)
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  function loginUser(event) {
+    event.preventDefault();
+    const form = event.target;
+    const userData = {
+      email: form.email.value,
+      password: form.password.value,
+    };
+    fetchLogIn(userData).then((data) => {
+      console.log(data);
+      dispatch(logIn({ token: data.result, ...data.user }));
+      window.localStorage.setItem("token", data.result);
+      event.target.reset();
+      navigate("/courses");
+    });
 
-            event.target.reset();
-            navigate("/courses");
-          })
-          .catch((error) => console.log(error));
-      }
+    console.log(userData);
+  }
   return (
     <div>
       <h3>Login</h3>

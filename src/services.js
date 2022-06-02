5d307bfb16fab62b8aa545c984e61d4ddacd52ff
VictoriaAnufrieva <victoria.anufrieva@gmail.com>
2022-06-02 10:50:09 +0200

    Update services.js

--- src/services.js
+++ src/services.js
@@ -2,34 +2,31 @@ export function fetchCourses() {
   return fetch("http://localhost:4000/courses/all")
     .then((res) => res.json())
     .then((data) => {
-       return data;
-      
+      return data;
     })
     .catch((error) => console.log(error));
 }
 
 export function fetchAuthors() {
-    return fetch("http://localhost:4000/authors/all")
-      .then((res) => res.json())
-      .then((data) => {
-         return data;
-        
-      })
-      .catch((error) => console.log(error));
-  }
-  
-  export function fetchLogIn(userData) {
-   return fetch("http://localhost:4000/login", {
-      method: "POST",
-      body: JSON.stringify(userData),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    })
-      .then((res) => res.json())
-      .then((data) => {
+  return fetch("http://localhost:4000/authors/all")
+    .then((res) => res.json())
+    .then((data) => {
       return data;
+    })
+    .catch((error) => console.log(error));
+}
 
-      })
-      .catch((error) => console.log(error));
- }
+export function fetchLogIn(userData) {
+  return fetch("http://localhost:4000/login", {
+    method: "POST",
+    body: JSON.stringify(userData),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      return data;
+    })
+    .catch((error) => console.log(error));
+}

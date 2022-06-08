e8c03e73a14dfb815172d922e4856aee43cb4607
VictoriaAnufrieva <victoria.anufrieva@gmail.com>
2022-06-08 15:38:32 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import { fetchAuthors, fetchCourses } from "./services";
 import "./App.css";
 import CourseInfo from "./components/CourseInfo/CourseInfo";
 import Courses from "./components/Courses/Courses";
-import CreateCourse from "./components/CreateCourse/CreateCourse";
+import CourseForm from "./components/CourseForm/CourseForm";
 import Header from "./components/Header/Header";
 import Login from "./components/Login/Login";
 import Registration from "./components/Registration/Registration";
@@ -13,18 +13,20 @@ import { mockedCoursesList } from "./constants";
 import { setAuthors } from "./store/authors/actionCreators";
 import { setCourses } from "./store/courses/actionCreators";
 import { coursesSelector } from "./store/courses/selectors";
+import { fetchAllCourses } from "./store/courses/thunk";
+import { fetchAllAuthors } from "./store/authors/thunk";
+import PrivateRoute from "./components/PrivateRoute/PrivateRoute";
 
 function App() {
   const dispatch = useDispatch();
   const courses = useSelector(coursesSelector);
   // const [courses, setCourses] = useState(mockedCoursesList);
   useEffect(() => {
-    fetchCourses().then((data) => {
-      dispatch(setCourses(data.result));
-    });
-    fetchAuthors().then((data) => {
-      dispatch(setAuthors(data.result));
-    });
+    dispatch(fetchAllCourses());
+    dispatch(fetchAllAuthors());
+    // fetchAuthors().then((data) => {
+    //   dispatch(setAuthors(data.result));
+    // });
   }, []);
 
   // const [isShownCreateCourse, setIsShownCreateCourse] = useState(false);
@@ -35,9 +37,22 @@ function App() {
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/registration" element={<Registration />} />
-        <Route path="/courses/add" element={<CreateCourse />} />
+        <Route
+          path="/courses/add"
+          element={
+            <PrivateRoute>
+              <CourseForm />
+            </PrivateRoute>
+          }
+        />
         <Route path="/courses" element={<Courses />} />
         <Route path="/courses/:courseId" element={<CourseInfo />} />
+        <Route path="/courses/update/:courseId" element={
+            <PrivateRoute>
+              <CourseForm />
+            </PrivateRoute>
+          }
+          />
       </Routes>
       {/* {!isShownCreateCourse ? (
         <Courses

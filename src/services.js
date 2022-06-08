dc12ee1c265d457bd3c9f36cf8863be2e7879ccb
VictoriaAnufrieva <victoria.anufrieva@gmail.com>
2022-06-08 15:36:28 +0200

    Update services.js

--- src/services.js
+++ src/services.js
@@ -1,5 +1,7 @@
 export function fetchCourses() {
-  return fetch("http://localhost:4000/courses/all")
+  return fetch("http://localhost:4000/courses/all", {
+    headers: { Authorization: localStorage.getItem("token") },
+  })
     .then((res) => res.json())
     .then((data) => {
       return data;
@@ -7,8 +9,24 @@ export function fetchCourses() {
     .catch((error) => console.log(error));
 }
 
+export function fetchCourse(courseId) {
+   return fetch(`http://localhost:4000/courses/${courseId}`, {
+     headers: { Authorization: localStorage.getItem("token") },
+   })
+     .then((res) => res.json())
+     .then((data) => {
+       return data;
+     })
+     .catch((error) => console.log(error));
+ }
+ 
+
 export function fetchAuthors() {
-  return fetch("http://localhost:4000/authors/all")
+  return fetch("http://localhost:4000/authors/all", {
+    headers: {
+      Authorization: localStorage.getItem("token"),
+    },
+  })
     .then((res) => res.json())
     .then((data) => {
       return data;
@@ -22,7 +40,95 @@ export function fetchLogIn(userData) {
     body: JSON.stringify(userData),
     headers: {
       "Content-Type": "application/json",
+      Authorization: localStorage.getItem("token"),
     },
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      return data;
+    })
+    .catch((error) => console.log(error));
+}
+
+export function fetchAddCourse(courseData) {
+  return fetch("http://localhost:4000/courses/add", {
+    method: "POST",
+    body: JSON.stringify(courseData),
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: localStorage.getItem("token"),
+    },
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      return data;
+    })
+    .catch((error) => console.log(error));
+}
+
+export function fetchAddAuthor(authorData) {
+  return fetch("http://localhost:4000/authors/add", {
+    method: "POST",
+    body: JSON.stringify(authorData),
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: localStorage.getItem("token"),
+    },
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      return data;
+    })
+    .catch((error) => console.log(error));
+}
+
+export function fetchUpdateCourse(courseId, updateData) {
+  return fetch(`http://localhost:4000/courses/${courseId}`, {
+    method: "PUT",
+    body: JSON.stringify(updateData),
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: localStorage.getItem("token"),
+    },
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      return data;
+    })
+    .catch((error) => console.log(error));
+}
+
+export function fetchUserData() {
+  return fetch("http://localhost:4000/users/me", {
+    headers: {
+      Authorization: localStorage.getItem("token"),
+    },
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      return data;
+    })
+    .catch((error) => console.log(error));
+}
+
+export function fetchDeleteCourse(courseId) {
+  return fetch(`http://localhost:4000/courses/${courseId}`, {
+    method: "DELETE",
+    headers: {
+      Authorization: localStorage.getItem("token"),
+    },
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      return data;
+    })
+    .catch((error) => console.log(error));
+}
+
+export function fetchLogOut() {
+  return fetch("http://localhost:4000/logout", {
+    method: "DELETE",
+    headers: { Authorization: localStorage.getItem("token") },
   })
     .then((res) => res.json())
     .then((data) => {

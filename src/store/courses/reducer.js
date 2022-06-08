8af78d617c24067df661233a2c6b2a7b97ab8efd
VictoriaAnufrieva <victoria.anufrieva@gmail.com>
2022-06-08 15:35:17 +0200

    Update courses reducer.js

--- src/store/courses/reducer.js
+++ src/store/courses/reducer.js
@@ -1,4 +1,9 @@
-import { SET_COURSES, DELETE_COURSE, ADD_COURSE } from "./actionTypes";
+import {
+  SET_COURSES,
+  DELETE_COURSE,
+  ADD_COURSE,
+  UPDATE_COURSE,
+} from "./actionTypes";
 const initialState = [];
 
 export default function coursesReducer(state = initialState, action) {
@@ -9,6 +14,13 @@ export default function coursesReducer(state = initialState, action) {
       return state.filter((course) => course.id !== action.payload);
     case ADD_COURSE:
       return [...state, action.payload];
+    case UPDATE_COURSE:
+      const courseIndex = state.findIndex(
+        (course) => course.id === action.payload.id
+      );
+      const newState = [...state];
+      newState[courseIndex] = action.payload;
+      return newState;
     default:
       return state;
   }

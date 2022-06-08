4c86e478ac3406e6d5e92fb264f837ddbdc9b999
VictoriaAnufrieva <victoria.anufrieva@gmail.com>
2022-06-08 15:34:11 +0200

    Update index.js

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,13 @@
-import { combineReducers, createStore } from "redux";
+import { applyMiddleware, combineReducers, createStore } from "redux";
 import coursesReducer from "./courses/reducer";
 import authorsReducer from "./authors/reducer";
 import userReducer from "./user/reducer";
-
+import thunk from "redux-thunk";
 
 const rootReducer = combineReducers({
-    courses: coursesReducer,
-    authors: authorsReducer,
-    user: userReducer,
+  courses: coursesReducer,
+  authors: authorsReducer,
+  user: userReducer,
 });
 
-export const store = createStore(rootReducer);
+export const store = createStore(rootReducer, applyMiddleware(thunk));
